acf52ae28a10921d71a30f8f12a99b7476635c9a
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-01-26 16:27:32 -0500

    Made the center planet clickable. Causes itself to spin the other direction.

--- Prog2A1EthanPowell/Assets/Scripts/SpinScript.cs
+++ Prog2A1EthanPowell/Assets/Scripts/SpinScript.cs
@@ -13,7 +13,7 @@ public class SpinScript : MonoBehaviour
 
     [SerializeField] float PlanetSize;
 
-    bool RotatingCW = true;
+    [SerializeField] bool RotatingCW = true;
 
     // Start is called before the first frame update
     void Start()
