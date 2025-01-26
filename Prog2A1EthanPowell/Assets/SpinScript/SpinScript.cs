fb4aefca3681f9778ae9e83df94c47d16620ff70
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-01-26 16:13:15 -0500

    Implemented clickable planets, still need to make the core planet clickable. Also added Kit's planet sprites to get art assets in and make sure the sprite sizes didn't break the click script.

--- Prog2A1EthanPowell/Assets/SpinScript/SpinScript.cs
+++ Prog2A1EthanPowell/Assets/SpinScript/SpinScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SpinScript : MonoBehaviour
 {
@@ -10,27 +11,67 @@ public class SpinScript : MonoBehaviour
     [SerializeField] Transform Planet;
     [SerializeField] Transform Root;
 
+    [SerializeField] float PlanetSize;
+
+    bool RotatingCW = true;
+
     // Start is called before the first frame update
     void Start()
     {
 
-        RevolutionSpeed = Random.Range(1, 8);
-        OrbitSpeed = Random.Range(1, 8);
+        RevolutionSpeed = Random.Range(.0025f, 0.04f);
+        OrbitSpeed = Random.Range(.0025f, 0.04f);
 
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
 
         Vector3 rootRotation = Root.eulerAngles;
         Vector3 planetRotation = Planet.eulerAngles;
 
-        rootRotation.z += RevolutionSpeed;
-        planetRotation.z += OrbitSpeed;
+        if (RotatingCW)
+        {
+
+            rootRotation.z -= RevolutionSpeed;
+            planetRotation.z -= OrbitSpeed;
+
+        }
+        else {
+
+            rootRotation.z += RevolutionSpeed;
+            planetRotation.z += OrbitSpeed;
+
+        }        
 
         Root.eulerAngles = rootRotation;
         Planet.eulerAngles = planetRotation;
 
+        //Debug.Log("The global location of the planet is " + Planet.position + " and the mouse is located at " + mousePos);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Debug.Log("Click!");
+
+            if (mousePos.x <= (Planet.position.x + (Planet.localScale.x)) && mousePos.x >= (Planet.position.x - (Planet.localScale.x)))
+            {
+
+                if (mousePos.y <= (Planet.position.y + (Planet.localScale.y)) && mousePos.y >= (Planet.position.y - (Planet.localScale.y)))
+                {
+
+                    RotatingCW = !RotatingCW;
+
+                }
+
+            }
+
+        }
+
     }
+
 }
